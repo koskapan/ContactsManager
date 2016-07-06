b400dc7351e4004dbdff392a902699fecad78fc3
koskapan <koskapan@gmail.com>
2016-07-06 17:23:52 +0300

    Added entity & repo interface

---
+++ ContactsManager.Domain/Abstract/IContactsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContactsManager.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Domain.Abstract
+{
+    public interface IContactsRepository
+    {
+        IEnumerable<Contact> Contacts { get; }
+    }
+}

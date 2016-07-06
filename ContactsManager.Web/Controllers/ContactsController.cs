7b7f4e20ad01a1811560eeb6031c10d124513442
koskapan <koskapan@gmail.com>
2016-07-06 17:14:24 +0300

    Added some route attributes

--- ContactsManager.Web/Controllers/ContactsController.cs
+++ ContactsManager.Web/Controllers/ContactsController.cs
@@ -1,37 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace ContactsManager.Web.Controllers
 {
+    [RoutePrefix("api/v1/contacts")]
     public class ContactsController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        [HttpGet]
+        [Route("all/{page_num=1}")]
+        public IEnumerable<string> Get(int page_num = 1, int page_size = 1)
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        [HttpGet]
         public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
+        [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
+        [HttpPut]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
+        [HttpDelete]
         public void Delete(int id)
         {
         }

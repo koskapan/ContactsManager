d1c85dbfa97ddc1127c8903e68eba9fafe59251d
koskapan <koskapan@gmail.com>
2016-07-18 11:19:19 +0300

    Added SignalR

---
+++ ContactsManager.Web/Hubs/ContactsHub.cs
@@ -0,0 +1,18 @@
+﻿using ContactsManager.Domain.Entity;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ContactsManager.Web.Hubs
+{
+    public class ContactsHub: Hub
+    {
+        public void Send(Contact entity)
+        {
+            Clients.AllExcept(Context.ConnectionId).sendData(entity);
+        }
+    }
+}
